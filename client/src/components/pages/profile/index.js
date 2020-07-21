198db99a0b9769333485d69eff15dbc82ec3aa2b
Alvaro Gomez <agcabrero3@gmail.com>
2020-07-21 18:08:16 +0200

    login-register version 1

--- client/src/components/pages/profile/index.js
+++ client/src/components/pages/profile/index.js
@@ -2,6 +2,12 @@
 import React, { Component } from 'react'
 import CoasterService from '../../../service/CoasterService'
 
+import index from '../profile/index'
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
 class ProfileForm extends Component{
     constructor(props) {
         super(props)
@@ -9,6 +15,8 @@ class ProfileForm extends Component{
             favourite:'',
             my_courses: '',
             role: '',
+            ad_item: '',
+            showModal: false
         }
         this.coasterService = new CoasterService()
     }
@@ -26,18 +34,50 @@ class ProfileForm extends Component{
             .catch(err => console.log(err))
     }
 
+    handleModalShow = () => this.setState({ showModal: true })
+    handleModalClose = () => this.setState({ showModal: false })
+
 
     render() {
         
-            return (
+        return (
             <>
-            <form onSubmit={this.handleFormSubmit}>
-            Favoritos <input onChange={this.handleInputChange} value={this.state.favourite} name="favourite" type="text" /> <br></br>
-            Mis Cursos <input onChange={this.handleInputChange} value={this.state.my_courses} name="my_courses" type="text" /> <br></br>
-            Rol <input onChange={this.handleInputChange} value={this.state.role} name="role" type="text" /> <br></br>
-            <input type="submit" value="Enviar" />
-            </form>
-            </>
+                <h1>Bienvenido </h1>
+                {
+                    this.props.loggedInUser && <Button onClick={this.handleModalShow} variant="dark" size="sm" style={{ marginBottom: '20px' }}>Crear nuevo item</Button>
+                }
+                <Modal show={this.state.showModal} onHide={this.handleModalClose}>
+
+            <Modal.Body>
+            <Form onSubmit= {this.handleFormSubmit}>
+                <Form.Group>
+                    <Form.Label>Favoritos</Form.Label>
+                    <Form.Control onChange={this.handleInputChange} value={this.state.favourite} name="favourite" type="text"/>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Mis Cursos</Form.Label>
+                    <Form.Control onChange={this.handleInputChange} value={this.state.my_courses} name="my_courses" type="text" />
+                    <Form.Text className="text-muted"> Cursos en los que estoy apuntado.</Form.Text>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Rol</Form.Label>
+                    <Form.Control onChange={this.handleInputChange} value={this.state.role} name="role" type="text" />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Añadir Ítem</Form.Label>
+                    <Form.Control onChange={this.handleInputChange} value={this.state.ad_item} name="ad_item" type="text"/>
+                    <Form.Text className="text-muted"> Añade un video, una foto, una noticia...</Form.Text>
+                </Form.Group>
+
+                <Button variant="dark" type="submit">Enviar</Button>
+                </Form>
+                </Modal.Body> 
+                </Modal>
+                </>
+                
             )
             
     }
